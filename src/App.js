def10103ba544d1c959b3adec020eee6e1243e3f
Siddharth Kothari <sidkothari005@gmail.com>
2022-03-08 14:11:53 +0530

    Counter App

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
+
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [counter, setCounter] = useState(0);
+
+  const increment = () => {
+    
+    setCounter(counter + 1)
+  }
+
+  const decrement = () => {
+    if (counter > 0) {
+      setCounter(counter - 1)
+    }
+    
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex flex-col items-center justify-center mt-40">
+      <h1 className='text-2xl text-white font-extrabold '>Simple Counter App</h1>
+      <div className='flex mt-5 items-end'>
+        <button onClick={decrement} className='text-black text-2xl font-semibold bg-white pr-2 pl-2 h-8 text-center '>-</button>
+        <h1 className='text-white font-semibold text-3xl mr-3 ml-3'>{counter}</h1>
+        <button onClick={increment} className='text-black text-2xl font-semibold bg-white pr-2 pl-2 h-8 text-center'>+</button>
+      </div>
+
     </div>
   );
 }
